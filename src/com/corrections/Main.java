bc0ee43d16a971613d184b95f6a5837ca8db0338
Michael <heymichaelw@gmail.com>
2017-07-17 14:48:03 -0400

    initial

---
+++ src/com/corrections/Main.java
@@ -0,0 +1,24 @@
+package com.corrections;
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+       Scanner scanner = new Scanner( System.in );
+       double operand1;
+       double operand2;
+       double sum;
+       double difference;
+       double division;
+       double multiplication;
+       double remainder;
+
+       System.out.println("Give me a number");
+       operand1 = Double.parseDouble(scanner.nextLine());
+       System.out.println("Give me another number");
+       operand2 = Double.parseDouble(scanner.nextLine());
+
+
+    }
+}
