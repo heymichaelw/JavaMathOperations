ae7c208789e5d8048ce52a045c1d77b457ac47e1
Michael <heymichaelw@gmail.com>
2017-07-18 09:16:44 -0400

    displaying results from two operands

--- src/com/corrections/Main.java
+++ src/com/corrections/Main.java
@@ -4,21 +4,31 @@ import java.util.Scanner;
 
 public class Main {
 
+    public static void showResults(double sum, double difference, double division, double multiplication, double remainder){
+        System.out.println("The sum is: " + sum);
+        System.out.println("The difference is: " + difference);
+        System.out.println("The product is: " + multiplication);
+        System.out.println("The quotient is: " + division);
+        System.out.println("After division, the remainder is: " + remainder);
+    }
+
     public static void main(String[] args) {
        Scanner scanner = new Scanner( System.in );
        double operand1;
        double operand2;
-       double sum;
-       double difference;
-       double division;
-       double multiplication;
-       double remainder;
 
        System.out.println("Give me a number");
        operand1 = Double.parseDouble(scanner.nextLine());
        System.out.println("Give me another number");
        operand2 = Double.parseDouble(scanner.nextLine());
 
+       double sum = operand1 + operand2;
+       double difference = operand1 - operand2;
+       double division = operand1 / operand2;
+       double multiplication = operand1 * operand2;
+       double remainder = operand1 % operand2;
+
+       showResults(sum, difference, division, multiplication, remainder);
 
     }
 }
